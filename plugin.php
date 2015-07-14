8e666c0e90d350aea1b39539da736acc02ffd6e2
Carl Fink <cafink@gmail.com>
2015-07-14 14:36:53 -0500

    Create time entry using cURL.

--- plugin.php
+++ plugin.php
@@ -7,6 +7,7 @@ class toggl extends SlackServicePlugin {
 	public $cfg = array('has_token' => true);
 
 	private $botname = 'togglebot';
+	private $toggl_userpass = '{api_token}:api_token';
 
 	function onView () {
 		return $this->smarty->fetch('view.txt');
@@ -16,6 +17,11 @@ class toggl extends SlackServicePlugin {
 
 		try {
 			list($duration, $description) = $this->parseInput($req['post']['text']);
+			$success = $this->createTimeEntry($duration, $description);
+			if ($success)
+				$this->postSuccess($req['post']['channel_id']);
+			else
+				$this->postError('Unable to create time entry.', $req['post']['channel_id']);
 		} catch (Exception $e) {
 			$this->postError($e->getMessage(), $req['post']['channel_id']);
 		}
@@ -32,9 +38,60 @@ class toggl extends SlackServicePlugin {
 	}
 
 	private function postError ($message, $channel) {
-		$this->postToChannel($message, array(
+		$this->postToChannel('Error: ' . $message, array(
 			'channel' => $channel,
 			'username' => $this->botname
 		));
+	}
+
+	private function postSuccess ($channel) {
+		$this->postToChannel('Time entry created successfully.', array(
+			'channel' => $channel,
+			'username' => $this->botname
+		));
+	}
+
+	private function createTimeEntry ($duration, $description) {
+
+		$data = array(
+			'time_entry' => array(
+				'description' => $description,
+				'duration' => $duration,
+				'start' => date('c'), // ISO 8601
+				'created_with' => 'Slack'
+			)
+		);
+
+		$data_json = json_encode($data);
+
+		$headers = array(
+			'Content-Type: application/json',
+			'Content-Length: ' . strlen($data_json),
+			'Authorization: Basic ' . base64_encode($this->toggl_userpass),
+		);
+
+		$curl = curl_init();
+
+		curl_setopt_array($curl, array(
+			CURLOPT_URL => 'https://www.toggl.com/api/v8/time_entries',
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_HTTPAUTH => CURLAUTH_BASIC,
+			CURLOPT_USERPWD => $this->toggl_userpass,
+			CURLOPT_HTTPHEADER => $headers,
+			CURLOPT_CAINFO => __DIR__ . '/cacert.pem', // Bundle of CA Root Certificates
+			CURLOPT_POSTFIELDS => $data_json
+		));
+
+		curl_exec($curl);
+		$info = curl_getinfo($curl);
+		//$error = curl_error($curl);
+
+		curl_close($curl);
+
+		// There was an error
+		if ($info['http_code'] != 200)
+			return false;
+
+		return true;
 	}
 }
