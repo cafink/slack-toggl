9af0e67301c0b16c964e7f732e4a90db2cfd69c6
Carl Fink <cafink@gmail.com>
2015-07-14 13:57:45 -0500

    Parse input and display error if too few arguments are provided.

--- plugin.php
+++ plugin.php
@@ -4,8 +4,9 @@ class toggl extends SlackServicePlugin {
 
 	public $name = 'Toggl Service';
 	public $desc = "Log your day's time via Slack commands.";
-
 	public $cfg = array('has_token' => true);
+
+	private $botname = 'togglebot';
 
 	function onView () {
 		return $this->smarty->fetch('view.txt');
@@ -13,9 +14,27 @@ class toggl extends SlackServicePlugin {
 
 	function onHook ($req) {
 
-		$this->postToChannel('Hello, world!', array(
-			'channel'   => '#test',
-			'username' => 'togglbot'
+		try {
+			list($duration, $description) = $this->parseInput($req['post']['text']);
+		} catch (Exception $e) {
+			$this->postError($e->getMessage(), $req['post']['channel_id']);
+		}
+	}
+
+	private function parseInput ($input) {
+		$words = explode(' ', $input, 3);
+
+		// 3 pieces required: trigger, duration, and description
+		if (count($words) < 3)
+			throw new Exception('Duration or description not provided.');
+
+		return array($words[1], $words[2]);
+	}
+
+	private function postError ($message, $channel) {
+		$this->postToChannel($message, array(
+			'channel' => $channel,
+			'username' => $this->botname
 		));
 	}
 }
